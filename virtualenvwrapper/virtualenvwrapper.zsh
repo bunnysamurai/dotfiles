fefc92df83224eae5beea6960ca82af27f176e40
amauragis <andrew@mauragis.com>
2016-03-24 16:32:47 -0400

    update stuff

--- virtualenvwrapper/virtualenvwrapper.zsh
+++ virtualenvwrapper/virtualenvwrapper.zsh
@@ -2,5 +2,8 @@ if [[ -a /usr/local/bin/virtualenvwrapper.sh ]]; then
   VIRTUALENVWRAPPER_PYTHON='/usr/bin/python3'
   export WORKON_HOME=~/.virtualenvs
   source /usr/local/bin/virtualenvwrapper.sh
+elif [[ -a /usr/bin/virtualenvwrapper.sh ]]; then
+  VIRTUALENVWRAPPER_PYTHON='/usr/bin/python3'
+  export WORKON_HOME=~/.virtualenvs
+  source /usr/bin/virtualenvwrapper.sh
 fi
-
