4d0381d0bae10b0ba71e35a463d6c060737b09be
amauragis <andrew@mauragis.com>
2016-04-26 14:38:37 -0400

    Added linux aliases for pbcopy

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,10 +1,14 @@
 # grc overides for ls
 #   Made possible through contributions from generous benefactors like
 #   `brew install coreutils`
-if $(gls &>/dev/null)
-then
+if $(gls &>/dev/null); then
   alias ls="gls -F --color"
   alias l="gls -lAh --color"
   alias ll="gls -l --color"
   alias la='gls -A --color'
+fi
+
+if [ !$(pbcopy &>/dev/null) ]; then
+  alias pbcopy='xclip -selection clipboard'
+  alias pbpaste='xclip -selection clipboard -o'
 fi
