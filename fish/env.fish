078713b143ec6e083bcb5515c015665efe87aea8
Andrew Mauragis <andrew@mauragis.com>
2018-10-26 15:22:06 -0400

    add browser env var

--- fish/env.fish
+++ fish/env.fish
@@ -1,6 +1,9 @@
 # Editor stuff 
 set -gx EDITOR 'vim' 
-set -gx VISUAL 'vim' 
+set -gx VISUAL 'vim'
+
+# default browser
+set -gx BROWSER google-chrome
  
 # Special snowflake variables 
 set -gx PROJECTS "$HOME/projects" 
