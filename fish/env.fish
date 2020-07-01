4deea5c56acf0516e6d24d31bbee13af51150fc8
Andy Mauragis <andrew@mauragis.com>
2020-07-01 16:10:36 -0400

    update stuff

--- fish/env.fish
+++ fish/env.fish
@@ -1,9 +1,10 @@
 # Editor stuff
 set -gx EDITOR 'vim'
 set -gx VISUAL 'vim'
+set -gx XEDITOR 'code'
 
 # default browser
-set -gx BROWSER google-chrome
+set -gx BROWSER firefox
 
 # Special snowflake variables
 set -gx PROJECTS "$HOME/projects"
