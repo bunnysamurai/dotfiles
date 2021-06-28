254334f05013505bb34fa880daebfca035a410f6
Andy Mauragis <andrew@mauragis.com>
2021-06-28 09:10:06 -0400

    Add cool memusg script, change browser env

--- fish/env.fish
+++ fish/env.fish
@@ -4,7 +4,7 @@ set -gx VISUAL 'vim'
 set -gx XEDITOR 'code'
 
 # default browser
-set -gx BROWSER firefox
+set -gx BROWSER google-chrome
 
 # Special snowflake variables
 set -gx PROJECTS "$HOME/projects"
