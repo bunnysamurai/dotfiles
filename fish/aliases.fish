81221e6f954a6fc8f76707428bcbd68906d196c2
Andy Mauragis <andrew@mauragis.com>
2022-03-22 13:06:50 -0400

    add sosym alias for listing exported symbols in an .so

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -8,3 +8,6 @@ alias gc 'git commit'
 alias gac 'git add -A; and git commit'
 alias gsu 'git submodule update --remote --merge'
 alias bbash 'set -lx NO_FISH 1; and exec bash'
+
+# List so symbols
+alias sosym "nm --demangle --dynamic --defined-only --extern-only"
