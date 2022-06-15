e72f9286245464cb2615d35b27620260a64e926d
Andy Mauragis <andrew@mauragis.com>
2022-06-15 13:36:00 -0400

    fix fish kraken script

--- fish/functions/kraken.fish
+++ fish/functions/kraken.fish
@@ -1,5 +1,10 @@
 
 function kraken --description "Open gitkraken"
-  set dir $argv[1] (pwd) # use a directory if you pass it in, otherwise cwd
+  # use a directory if you pass it in, otherwise cwd
+  if [ ! -z "$argv[1]" ]
+    set -f dir (realpath $argv[1])
+  else
+    set -f dir (pwd)
+  end
   bash -c "gitkraken -p $dir" # /usr/bin/gitkraken is a bash script without a shebang.
 end
