3c770bc33e2cdadd881e5fd80dabcf72e5545a31
Andy Mauragis <andrew@mauragis.com>
2022-07-26 16:50:21 -0400

    wezterm

--- fish.config/env.fish
+++ fish.config/env.fish
@@ -6,7 +6,8 @@ set -gx XEDITOR 'code'
 # default browser
 set -gx BROWSER google-chrome
 
+set -gx TERMINAL wezterm
+
 # Special snowflake variables
 set -gx PROJECTS "$HOME/projects"
 set -gx DOTFILES "$HOME/.dotfiles"
-
