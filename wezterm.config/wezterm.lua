82fb85d133e8142515f4557141e0512b92caf29b
Andy Mauragis <andrew@mauragis.com>
2022-08-01 13:56:49 -0400

    wezterm, pt 2

--- wezterm.config/wezterm.lua
+++ wezterm.config/wezterm.lua
@@ -1,10 +1,27 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
+local util = require("util")
+local keybinds = require("keybinds")
 
-return {
-    font = wezterm.font 'Iosevka Term',
-    launch_menu = {{
-        args = {'btop'}
-    }},
+-- set term to wezterm if the def exists.
+-- disabled because it makes ssh kind of a chore
+local term_cfg = "xterm-256color"
+-- if string.find(wezterm.target_triple, 'linux') ~= nil then
+--     if os.execute('infocmp wezterm > /dev/null 2>&1') then
+--         term_cfg = "wezterm"
+--     end
+-- end
+
+local font_cfg = wezterm.font_with_fallback({ "Iosevka Term", "JetBrains Mono", "Noto Color Emoji",
+    "Symbols Nerd Font Mono", "Last Resort High-Efficiency" })
+
+local config = {
+    automatically_reload_config = true,
+    term = term_cfg,
+    font = font_cfg,
+    font_size = 12,
+    launch_menu = { {
+        args = { "btop" }
+    } },
     colors = {
         foreground = "#bbbbbb",
         background = "#121212",
@@ -14,9 +31,21 @@ return {
         selection_bg = "#b5d5ff",
         selection_fg = "#000000",
 
-        ansi = {"#121212", "#fa2573", "#98e123", "#dfd460", "#1080d0", "#8700ff", "#43a8d0", "#bbbbbb"},
-        brights = {"#555555", "#f6669d", "#b1e05f", "#fff26d", "#00afff", "#af87ff", "#51ceff", "#ffffff"}
+        ansi = { "#121212", "#fa2573", "#98e123", "#dfd460", "#1080d0", "#8700ff", "#43a8d0", "#bbbbbb" },
+        brights = { "#555555", "#f6669d", "#b1e05f", "#fff26d", "#00afff", "#af87ff", "#51ceff", "#ffffff" },
 
-    }
-
+        visual_bell = '#333300',
+    },
+    leader = keybinds.leader,
+    keys = keybinds.keys,
+    hide_tab_bar_if_only_one_tab = true,
+    visual_bell = {
+        fade_in_function = 'Linear',
+        fade_in_duration_ms = 40,
+        fade_out_function = 'EaseOut',
+        fade_out_duration_ms = 150,
+    },
+    audible_bell = "Disabled"
 }
+
+return config
