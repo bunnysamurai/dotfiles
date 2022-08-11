8a136dc4733df9155b142eb0de1eda1c664e82f3
Andy Mauragis <andrew@mauragis.com>
2022-08-11 10:40:21 -0400

    fcd improvements

--- fish.config/fzf.fish
+++ fish.config/fzf.fish
@@ -24,10 +24,40 @@ function kp --description "Kill processes"
     end
 end
 
-function fcd --description "fuzzy change directory" -a searchdir
-    # set -q searchdir[1]; and echo "searchdir is set: $searchdir"; or echo "searchdir is unset"
-    set -q searchdir[1]; or set searchdir '.'
-    set -l dir (fd --type d . $searchdir | fzf)
+function fcd --description "fuzzy change directory"
+    argparse 'h/help' 'H/hidden' 'I/no-ignore' -- $argv
+
+    if set -q _flag_help
+        echo "\
+fcd - fuzzy change directory
+USAGE
+  fcd [OPTS] <path>
+
+OPTS:
+  -h/--help      - print this help
+  -H/--hidden    - include hidden files in output
+  -I/--no-ignore - include files that are typically ignored by `fd` in output
+
+ARGS:
+  <path>         - path to start cd search from (default .)
+"
+        return 0
+    end
+
+    set -f fd_args
+    if set -q _flag_hidden
+        set -a fd_args '-H'
+    end
+    if set -q _flag_no_ignore
+        set -a fd_args '-I'
+    end
+
+    set -f searchdir '.'
+    if set -q argv[1]
+        set -f searchdir $argv[1]
+    end
+    set -l dir (fd $fd_args --type d . $searchdir | fzf)
+    # set -l
     if set -q dir[1]
         cd $dir
     end
